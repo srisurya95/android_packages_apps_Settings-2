c59927227fce04ed7b5a56e902b2805f34605964
d34d <clark@cyngn.com>
2015-06-16 10:46:53 -0700

    Don't clear cache on null UiDetailProvider
    When viewing data usage, press the search button, rotate to landscape
    then rotate back to portrait.  This will cause settings to crash
    with a NullPointerException due to mUiDetailProvider being null when
    onDestroy() is called.
    
    Change-Id: I27f4a633a15281285657b946495b11ebc84ff66d

--- src/com/android/settings/DataUsageSummary.java
+++ src/com/android/settings/DataUsageSummary.java
@@ -613,8 +613,10 @@ public class DataUsageSummary extends HighlightingFragment implements Indexable 
         mDataEnabledView = null;
         mDisableAtLimitView = null;
 
-        mUidDetailProvider.clearCache();
-        mUidDetailProvider = null;
+        if (mUidDetailProvider != null) {
+            mUidDetailProvider.clearCache();
+            mUidDetailProvider = null;
+        }
 
         TrafficStats.closeQuietly(mStatsSession);
 
