8612ad8a114c95e2e7b37cab72c206f14e5baa4a
hashem78 <hashem.olayano@gmail.com>
2015-06-21 14:01:59 +0300

    Settings : call the correct string .

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -81,7 +81,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
     private static final String KEY_SAFETY_LEGAL = "safetylegal";
-    private static final String KEY_PA_VERSION = "pa_version";
+    private static final String KEY_MERK_VERSION = "merk_version";
     private static final String KEY_MOD_BUILD_DATE = "build_date";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
@@ -104,8 +104,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
-        findPreference(KEY_PA_VERSION).setEnabled(true);
-        setValueSummary(KEY_PA_VERSION, "ro.pa.version");
+        findPreference(KEY_MERK_VERSION).setEnabled(true);
+        setValueSummary(KEY_MERK_VERSION, "ro.merk.version");
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
 
         if (!SELinux.isSELinuxEnabled()) {
